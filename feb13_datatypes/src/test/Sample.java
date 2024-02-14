2fad7f584bc90ffc329e921f4de5084d7ed44776
sulaiman114 <mohamedsulaimanf114@gmail.com>
2024-02-14 22:02:45 +0530

    Initial commit

---
+++ feb13_datatypes/src/test/Sample.java
@@ -0,0 +1,32 @@
+package test;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte xyz= -128;
+		System.out.println(xyz);
+		
+		short x1= -32768;
+		System.out.println(x1);
+		
+		boolean x2= true;
+		System.out.println(x2);
+		
+		int x3= 1234567891;
+		System.out.println(x3);
+		
+		long x4=12345678912l;
+		System.out.println(x4);
+
+		
+		float x5=11.1f;
+		System.out.println(x5);
+		
+		
+		double x6= 1.23445d;
+		System.out.println(x6);
+		
+	}
+
+}
