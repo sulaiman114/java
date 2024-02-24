15237c958bde762ea474475e353800d6fb8b3be6
sulaiman114 <mohamedsulaimanf114@gmail.com>
2024-02-24 20:37:42 +0530

    methodA
    learnmethod

---
+++ javamethod/src/test/SampleA.java
@@ -0,0 +1,23 @@
+package test;
+
+public class SampleA {
+
+	public static void main(String[] args) {
+		
+		System.out.println("execution");
+		again();
+		again();
+		repeat();
+		again();
+		
+
+	}
+private static void again()
+{
+System.out.println("new one");
+}
+public static void repeat()
+{
+	System.out.println("example for learning");
+}
+}
