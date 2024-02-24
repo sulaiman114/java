062ed2d96ddc1f26255779fe9a73bab314f1d5d4
sulaiman114 <mohamedsulaimanf114@gmail.com>
2024-02-24 22:15:15 +0530

    method return
    method return

---
+++ methodreturn/src/test/SampleA.java
@@ -0,0 +1,25 @@
+package test;
+
+public class SampleA {
+
+	public static void main(String[] args) {
+		
+	addtwovalue(90,10);
+	addtwovalue(10,20);
+	addthree(10,10,10);
+	addthree(88,1,1);
+	
+
+	}
+public static void addtwovalue(int m,int n)
+{
+	System.out.println("adddtioin..!");
+	System.out.println(m+n);
+	
+}
+public static void addthree(int l,int m,int n)
+{
+	System.out.println("adddtioin..!");
+	System.out.println(l+m+n);
+}
+}
