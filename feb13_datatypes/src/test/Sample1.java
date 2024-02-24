c9ac8268d3d048ff911fdc661bacf9824424dd22
sulaiman114 <mohamedsulaimanf114@gmail.com>
2024-02-24 10:44:20 +0530

    ifelse
    ifelse

---
+++ feb13_datatypes/src/test/Sample1.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			byte xyz= 57;
+			System.out.println(xyz);
+			xyz=64;
+			System.out.println(xyz);
+
+	}
+
+}
---
+++ feb22_ifelse/src/test/SampleA.java
@@ -0,0 +1,24 @@
+package test;
+
+public class SampleA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=100;
+		int b=150;
+		System.out.println("start execution");
+		
+		if(a>b)
+		{
+			System.out.println("a is high");
+		}
+		
+		else
+		{
+			System.out.println("b is lesse");
+		}
+
+	}
+
+
+}
