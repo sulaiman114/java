4600321a022ecca7dbabf2763203c372ee2113c6
sulaiman114 <mohamedsulaimanf114@gmail.com>
2024-02-26 21:54:06 +0530

    switch statement
    switch statement

---
+++ feb22_switchstatement/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module feb22_switchstatement {
+}
---
+++ feb22_switchstatement/src/test/Sample.java
@@ -0,0 +1,29 @@
+package test;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x=3;
+		switch(x)
+		{
+		case 1:
+			System.out.println("sun");
+			break;
+		
+		case 2:
+			System.out.println("mon");
+			break;
+		case 3:
+			System.out.println("tues");
+			break;
+		case 4:
+			System.out.println("wed");
+			break;
+			default:
+				System.out.println("no name");
+				
+	}
+	}
+}
