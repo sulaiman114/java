3fc835a75984264f24c10f9594142908835899f4
sulaiman114 <mohamedsulaimanf114@gmail.com>
2024-03-16 14:06:53 +0530

    sample
    selenium

---
+++ march14_sele/src/test/java/test/SampleA.java
@@ -0,0 +1,23 @@
+package test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class SampleA {
+
+	public static void main(String[] args) {
+		WebDriver driver; 
+		driver = new ChromeDriver();
+	
+		driver.get("https://demo.automationtesting.in/Register.html");
+		//driver.navigate().forward();
+		
+		//Select dropDownSkill= new Select(driver.findElement(By.xpath("//select[@id='Skills']")));
+		//dropDownSkill.selectByIndex(6);
+		driver.manage().window().minimize();		
+	}
+	
+
+}
